9aa403545116e6ee3d5597f1733dca94a4807af2
Елена <lenna@gmail.com>
2023-08-20 01:46:34 +0300

    Added program in Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 42 
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 46 -> 101110
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+//int binintdec = DecimaltoBinary(number);
+Console.WriteLine($"Десятичное число преобразованное в двоичное = {DecimaltoBinary(number)}");
+
+// int DecimaltoBinary(int num)
+// {
+//     int count = 0;
+//     int basedec = 1;
+//     while (num > 0)
+//     {
+//         count += num % 2 * basedec;
+//         num /= 2;
+//         basedec *= 10;
+
+//     }
+//     return count;
+// }
+
+string DecimaltoBinary(int num)
+{
+    string count = string.Empty;
+    while (num > 0)
+    {
+        count = num % 2 + count;
+        num /= 2;
+
+
+    }
+    return count;
+}
