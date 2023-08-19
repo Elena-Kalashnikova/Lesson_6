95e9622525f9720e3f7e6ca7dbf17a381fac39b0
Елена <lenna@gmail.com>
2023-08-20 00:41:46 +0300

    Added program in Task39

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернет одномерный массив 
+// [12345] = [54321]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+
+
+}
+
+int[] array = CreateArrayRndInt(5, 1, 10);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
+// Array.Reverse(array);
+// PrintArray(array);
+
